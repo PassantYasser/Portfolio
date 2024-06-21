811c774b523add2d88e06a2c963187be591d48a2
PassantYasser <114351321+PassantYasser@users.noreply.github.com>
2024-06-22 00:59:07 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,31 @@
+
+const button1 = document.getElementById('facelink');
+button1.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  window.open('https://www.facebook.com/bassant.abutalib/', '_blank');
+});
+
+const button2 = document.getElementById('linkedinlink');
+button2.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  window.open('https://www.linkedin.com/in/passant-yasser-806118256/', '_blank');
+});
+
+const button3 = document.getElementById('gitlink');
+button3.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  window.open('https://github.com/PassantYasser', '_blank');
+});
+const email = document.getElementById('email');
+email.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  window.open('passantabutalib@gmail.com', '_blank');
+});
+
+/////////////////////////////////////
+
+
